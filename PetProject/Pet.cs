71e964b9c1846f989a44b6a7c539035e9d5e15c5
Aaron Hinkle <95721634+AaronHinkle@users.noreply.github.com>
2023-06-30 14:43:01 -0400

    Add project files.

---
+++ PetProject/Pet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetProject
+{
+    public abstract class Pet
+    {
+        //fields
+        private string name;
+        private string species;
+        private int hunger;
+        private int boredom;
+        private int health;
+        private bool isRobot;
+
+
+
+        // properties
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Hunger { get; set; }
+        public int Boredom { get; set; }
+        public int Health { get; set; }
+        public bool IsRobot { get; set; }
+        //constructor
+
+        public Pet(string name, string species, int hunger, int boredom, int health, bool isRobot)
+        {
+            Name = name;
+            Species = species;
+            Hunger = hunger;
+            Boredom = boredom;
+            Health = health;
+            IsRobot = isRobot;
+        }
+        public abstract void Feed();
+
+
+
+
+
+        public abstract void SeeDoctor();
+
+
+
+        public abstract void Play();
+
+
+
+        public abstract void GetStatus();
+
+
+
+        public abstract void Tick();
+
+
+        public abstract void Speak();
+
+
+
+    }
+
+}
+
+
